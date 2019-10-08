b9950fa52a8ca7b1302928d5845739b2ac482c47
rafael <deluca1712@gmail.com>
2019-10-08 14:28:19 -0300

    pilha

--- src/application/Pilha.java
+++ src/application/Pilha.java
@@ -3,9 +3,25 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class try_catch {
+public class Pilha {
 
 	public static void main(String[] args) {
+
+		metodo1();
+
+		System.out.println("Fim do programa");
+	}
+
+	public static void metodo1() {
+		System.out.println("***MÉTODO UM START***");
+		metodo2();
+		System.out.println("***MÉTODO UM END***");
+
+	}
+
+	public static void metodo2() {
+
+		System.out.println("***MÉTODO DOIS START***");
 		Scanner sc = new Scanner(System.in);
 		try {
 			System.out.println("Informe uma lista de valores separados por virgula e depois"
@@ -17,15 +33,19 @@ public class try_catch {
 
 		} catch (ArrayIndexOutOfBoundsException erroPosicaoInvalida) {
 			System.out.println("Tu informaste uma posicão que não existe dados.");
+			//imprime na tela o rastreamento do stack
+			//por default do java chama printstacktrace para te ajudar a achar o erro de exceção não tratada 
+			erroPosicaoInvalida.printStackTrace();
 
 		}
 
 		catch (InputMismatchException erroTipoDeDados) {
 			System.out.println("Tu não informaste um número inteiro.");
+			erroTipoDeDados.printStackTrace();
 		}
 
-		
 		sc.close();
+		System.out.println("***MÉTODO DOIS END***");
 
 	}
 
