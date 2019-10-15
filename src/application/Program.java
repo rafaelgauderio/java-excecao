79d4ebe9e53095fd6cb65154a2525f35ce196c4a
rafael <deluca1712@gmail.com>
2019-10-15 13:56:14 -0300

    Tratanto exceções com exceções padrões do java

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,48 +15,42 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		try {
 
-		System.out.println("Infome o númedo do quarto e data de check-in e check-out (dd/MM/yyyy):");
-		int numeroQuarto = sc.nextInt();
-		// converte String para Date
-		Date checkin = sdf.parse(sc.next());
-		Date checkout = sdf.parse(sc.next());
+			System.out.println("Infome o númedo do quarto e data de check-in e check-out (dd/MM/yyyy):");
+			int numeroQuarto = sc.nextInt();
+			// converte String para Date
+			Date checkin = sdf.parse(sc.next());
+			Date checkout = sdf.parse(sc.next());
 
-		// Fazer as validaoes na propria aplicação é muito ruim, o correto é
-		// fazer na classe reseva
-
-		if (!checkout.after(checkin)) {
-			System.out.println("Erro. A data de saída tem que ser posterios a data de chegada.");
-		}
-
-		else {
 			Reserva reserva = new Reserva(numeroQuarto, checkin, checkout);
 			System.out.println(reserva);
-
 			System.out.println();
 
+			// comandos testes
+			System.out.println("Print da segunda reserva: (teste)");
+			Reserva reserva2 = new Reserva(2536090, sdf.parse("17/12/2019"), sdf.parse("25/12/2029"));
+			System.out.println("O número do quarto da 2.ª reserva é: " + reserva2.getNumeroQuarto());
+			reserva2.setNumeroQuarto(2560);
+			System.out.println("2ª reserva: " + reserva2);
+			System.out.println();
 			System.out.println("Informe as novas de checkin e checkout da reserva:");
 			checkin = sdf.parse(sc.next());
 			checkout = sdf.parse(sc.next());
-
 			// fazer a data atual
 			Date hoje = new Date();
-			if (checkout.before(hoje) || checkin.before(hoje)) {
-				System.out.println("As revervas tem que ser em datas futuras");
-
-			} else if (!checkout.after(checkin)) {
-				System.out.println("Erro. A data de saída tem que ser posterios a data de chegada.");
-			}
-
-			else {
-				reserva.atualizarDatas(checkin, checkout);
-				System.out.println("Dados atualizados da " + reserva);
-
-				sc.close();
-			}
+			reserva.atualizarDatas(checkin, checkout);
+			System.out.println("Dados atualizados da " + reserva);
+		} catch (ParseException erroCaracterInvalido) {
+			System.out.println("Data no formato inválido");
+		} catch (IllegalArgumentException erroArgumentoInvalido) {
+			System.out.println("Erro na reserva: " + erroArgumentoInvalido.getMessage());
 
 		}
 
+		sc.close();
 	}
 
 }
+
+
--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -5,66 +5,71 @@ import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reserva {
-	
+
 	private Integer numeroQuarto;
 	private Date checkin;
 	private Date checkout;
-	public static SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/yyyy");
-	
-public Reserva(Integer numeroQuarto, Date checkin, Date checkout) {
+	public static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+	public Reserva(Integer numeroQuarto, Date checkin, Date checkout) {
 		this.numeroQuarto = numeroQuarto;
 		this.checkin = checkin;
 		this.checkout = checkout;
-	}	
-public Reserva () {
-	
-}
-public Integer getNumeroQuarto() {
-	return numeroQuarto;
-}
-public void setNumeroQuarto(Integer numeroQuarto) {
-	this.numeroQuarto = numeroQuarto;
-}
-public Date getCheckin() {
-	return checkin;
-}
-public void setCheckin(Date checkin) {
-	this.checkin = checkin;
-}
-public Date getcheckout() {
-	return checkout;
-}
-public void setCheckout(Date checkout) {
-	this.checkout = checkout;
-}
-	
-public Long duracao() {
-	//converter de data para inteiro para poder fazer a conta
-	Long diferenca=checkout.getTime()- checkin.getTime();
-	//converter de milisegundos (pradrao do time Unit para dias)
-	return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
-		
-}
+	}
 
-public void atualizarDatas(Date checkin, Date checkout) {
-	this.checkin=checkin;
-	this.checkout=checkout;
-}
+	public Reserva() {
 
-@Override
-public String toString() {
-	return "Reserva: Número do quarto: "
-			+ numeroQuarto
-			+ ", check-in: "
-			+sdf.format(checkin)
-			+", check-out: "
-			+sdf.format(checkout)
-			+", "
-			+duracao()
-			+" dias."
-;
-}
+	}
 
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+
+	public Date getCheckin() {
+		return checkin;
+	}
+
+	public void setCheckin(Date checkin) {
+		this.checkin = checkin;
+	}
+
+	public Date getcheckout() {
+		return checkout;
+	}
+
+	public void setCheckout(Date checkout) {
+		this.checkout = checkout;
+	}
+
+	public Long duracao() {
+		// converter de data para inteiro para poder fazer a conta
+		Long diferenca = checkout.getTime() - checkin.getTime();
+		// converter de milisegundos (pradrao do time Unit para dias)
+		return TimeUnit.DAYS.convert(diferenca, TimeUnit.MILLISECONDS);
+
+	}
+
+	public void atualizarDatas(Date checkin, Date checkout) {
+		Date agora = new Date();
+		if (checkin.before(agora) || checkout.before(agora)) {
+			throw new IllegalArgumentException("As datas das reservas devem ser datas futuras.");
+		}
+		if (!checkout.after(checkin)) {
+			throw new IllegalArgumentException("A data de check-out deve ser posterior a de check-in.");
+		}
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	@Override
+	public String toString() {
+		return "Reserva: Número do quarto: " + numeroQuarto + ", check-in: " + sdf.format(checkin) + ", check-out: "
+				+ sdf.format(checkout) + ", " + duracao() + " dias.";
+	}
 	
 }
 
